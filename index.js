295e1d8b0b86e3ce5105fe8d321c0a6f4558cf7e
zakie <zakie@klp2.fantastic.com>
2021-04-17 14:41:57 +0700

    commit pertama cuy

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
+
+router.use("/products", (req, res, next) => {
+  console.log("request: ", req);
+});
+
+app.use("/", router);
 
 app.use(() => {
   console.log("hello server");
